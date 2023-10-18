0a299eea3d716100ffbabb0ec30e6cece5217476
Beatrice Beacco <beatrice.beacco@gmail.com>
2023-10-18 10:15:52 +0200

    template

---
+++ spring-backend/src/main/java/com/example/azure_login/AzureLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureLoginApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/com/example/azure_login/AzureLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azure_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
