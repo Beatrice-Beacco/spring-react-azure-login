5f057cf76cd7cfca4a49893eede04d1cf5725b27
Beatrice Beacco <beatrice.beacco@gmail.com>
2023-10-18 11:50:25 +0200

    feat: react  config setup

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./authConfig.ts";
+import { MsalProvider } from "@azure/msal-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+/**
+ * MSAL should be instantiated outside of the component tree to prevent it from being re-instantiated on re-renders.
+ * For more, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+ */
+const msalInstance = new PublicClientApplication(msalConfig);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);
